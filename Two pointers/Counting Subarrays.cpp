210d5ebd127f16491cdf00dfb122e0db4ae7de74
MaginMadhavan <76735710+MaginMadhavan@users.noreply.github.com>
2022-05-17 18:37:18 +0530

    Create Counting Subarrays.cpp

---
+++ Two pointers/Counting Subarrays.cpp
@@ -0,0 +1,15 @@
+int Solution::solve(vector<int> &A, int B) {
+    int n=A.size(),count=0,sum=0,j=0;
+
+    for(int i=0;i<n;i++)
+    {
+        sum+=A[i];
+        while(sum>=B)
+        {
+            sum-=A[j];
+            j++;
+        }
+        count+=i-j+1;
+    }
+    return count;
+}
