5fb08cff1e79ff768478f033eea5b2c15dafec3a
MaginMadhavan <76735710+MaginMadhavan@users.noreply.github.com>
2022-05-19 15:10:46 +0530

    Update Find permutation.cpp

--- Array/Find permutation.cpp
+++ Array/Find permutation.cpp
@@ -1,21 +1,21 @@
 vector<int> Solution::findPerm(const string A, int B) {
-    int s=1;
-    int l=B;
+    int small=1;
+    int large=B;
     vector<int>v;
     for(int i=0;i<A.size();i++)
     {
         if(A[i]=='I')
         {
-            v.push_back(s);
-            s++;
+            v.push_back(small);
+            small++;
         }
         if(A[i]=='D')
         {
-            v.push_back(l);
-            l--;
+            v.push_back(large);
+            large--;
         }
     }
     
-    v.push_back(l);
+    v.push_back(large);
     return v;
 }
