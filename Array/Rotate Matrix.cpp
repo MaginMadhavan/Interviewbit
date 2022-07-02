bc499d9831c91d4cad2509093319c6a2ff793b42
MaginMadhavan <76735710+MaginMadhavan@users.noreply.github.com>
2022-07-02 14:59:42 +0530

    Update Rotate Matrix.cpp

--- Array/Rotate Matrix.cpp
+++ Array/Rotate Matrix.cpp
@@ -1,28 +1,15 @@
 void Solution::rotate(vector<vector<int> > &A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-
-
-    //transpose of matrix
-    for(int i=0;i<A.size();i++)
+    
+    int m=A.size(),n=A[0].size();
+    
+    //transpose of a matrix
+    for(int i=0;i<m;i++)
     {
-        for(int j=i;j<A[0].size();j++)
-        {
-            if(j!=i)
-            {
-                swap(A[j][i],A[i][j]);
-            }
-        }
-    }
-
-    for(int i=0;i<A.size();i++)
-    {
-        for(int j=0;j<A[0].size()/2;j++)
-        {
-            swap(A[i][j],A[i][A.size()-1-j]);
-        }
+        for(int j=0;j<i;j++) swap(A[i][j],A[j][i]);   
     }
     
+    
+    //reverse each row
+    for(int i=0;i<m;i++) reverse(A[i].begin(),A[i].end());
+     
 }
