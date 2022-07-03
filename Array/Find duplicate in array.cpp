38b54c0541cfef44027781cadedf5c06e822040b
MaginMadhavan <76735710+MaginMadhavan@users.noreply.github.com>
2022-07-03 14:51:46 +0530

    Update Find duplicate in array.cpp

--- Array/Find duplicate in array.cpp
+++ Array/Find duplicate in array.cpp
@@ -5,17 +5,26 @@ int Solution::repeatedNumber(const vector<int> &A) {
     {
         if(v[A[i]]==1)return A[i];
         v[A[i]]=1;
-    }*/
-
-    vector<int>v(A.size()+1,0);
-    for(int i=0;i<A.size();i++)
-    {
-        v[A[i]]=v[A[i]]+1;
     }
-    for(int i=0;i<v.size();i++)
-    {
-        if(v[i]>1)return i;
-    }
+ return -1;*/
  
- return -1;
+    //Tortoise and the hare
+    int slow=A[0],fast=A[0];
+    int n=A.size();
+    
+    do
+    {
+        slow=A[slow];
+        fast=A[A[fast]];
+    }while(slow!=fast);
+    
+    fast=A[0];
+    while(fast!=slow)
+    {
+        slow=A[slow];
+        fast=A[fast];
+    }
+    return slow;
+    
 }
+
